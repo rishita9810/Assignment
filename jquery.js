6df146cc67f1b5fd7a3a65c924864cfd6c856030
rishita9810 <42089682+rishita9810@users.noreply.github.com>
2020-10-17 12:09:23 +0530

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -2,9 +2,7 @@ $(document).ready(function () {
     var currentDate = new Date();
     console.log(currentDate);
     var currentDay = ("0" + currentDate.getDate()).slice(-2);
-    var currentMonth = ("0" + (currentDate.getMonth() + 1)).slice(-2);
-    var currentYear = currentDate.getFullYear();
-    var finalDate = currentYear + "-" + currentMonth + "-" + currentDay;
+    var currentMonth = ("0" + (currentDate.getMonth() + 1)).slice(-2);    var finalDate = currentYear + "-" + currentMonth + "-" + currentDay;
     $(".row").find("#checkInDate").val(finalDate).attr("min", finalDate);
     $(".row").find("#checkOutDate").val(finalDate).attr("min", finalDate);
     function getRoomCode() {
